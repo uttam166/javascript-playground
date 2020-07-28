b582e68c8e53ea21676e7aad83bf5ed64f53da53
uttamkrsingh <uttam166@gmail.com>
2020-07-28 18:27:34 +0530

    file updated by node

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
 
-fs.writeFileSync('note.txt', 'This file created by node js.');
+// fs.writeFileSync('note.txt', 'This file created by node js.');
+fs.appendFileSync('note.txt', ' Now this file is updated.');
