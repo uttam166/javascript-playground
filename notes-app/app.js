49839ecbb611948efbe4e95ff2a61ce584ff941f
uttamkrsingh <uttam166@gmail.com>
2020-07-29 18:09:57 +0530

    argument parsing with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,45 @@
 const getNotes = require('./note.js');
+const yargs = require('yargs');
 
-const command = process.argv[2]
+// console.log(process.argv);
 
-if(command === 'add'){
-    console.log('addning notes.')
-}else if(command === 'remove'){
-    console.log('removing a note.')
-}
+yargs.version('1.1.1');
+
+//add a note
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    handler: function(){
+        console.log('adding a new note.')
+    }
+});
+
+// remove a note
+yargs.command({
+    command: 'remove',
+    describe: 'remove a new note',
+    handler: function(){
+        console.log('removeing a new note.')
+    }
+});
+
+//list a note
+yargs.command({
+    command: 'list',
+    describe: 'list a new note',
+    handler: function(){
+        console.log('listing a new note.')
+    }
+});
+
+// read a note 
+yargs.command({
+    command: 'read',
+    describe: 'read a new note',
+    handler: function(){
+        console.log('reading a new note.')
+    }
+});
+
+
+console.log(yargs.argv);
