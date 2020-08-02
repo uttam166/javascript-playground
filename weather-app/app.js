c782cd80718e75c88e9a0cbb9aae6d4de9f15b06
uttamkrsingh <uttam166@gmail.com>
2020-08-02 18:18:43 +0530

    get  geocodinates from mapbox api

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,5 +7,15 @@ request({ url , json: true}, ( error, response ) => {
     const temp = response.body.main.temp + " degree ";
     const feels_like = response.body.main.feels_like + " degree " ;
     const wind = response.body.wind.speed + " meter/sec ";
-    console.log(wind);
+    // console.log(wind);
+})
+
+const geocodeUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/siliguri.json?access_token=pk.eyJ1IjoidXR0YW0xNjYiLCJhIjoiY2tkZDE3NjNjMTNjajJybXpxNXdjeGZzbSJ9.GAuUH-nff7spTLs5U8OHHw&limit=1";
+
+request({ url: geocodeUrl , json: true}, ( error, response ) => {
+   const place_name = response.body.features[0].place_name;
+   const longitude = response.body.features[0].center[0];
+   const latitude = response.body.features[0].center[1];
+   
+   console.log(longitude,latitude);
 })
